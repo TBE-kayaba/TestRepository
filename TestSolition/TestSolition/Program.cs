d77d226df464dd3e14e1634a8aa848b2345d7ef1
TBE-kayaba <t-kayaba@tbeye.com>
2021-09-01 14:28:06 +0900

    devプッシュ

--- TestSolition/TestSolition/Program.cs
+++ TestSolition/TestSolition/Program.cs
@@ -17,6 +17,7 @@ namespace TestSolition
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // コメント
         }
     }
 }
